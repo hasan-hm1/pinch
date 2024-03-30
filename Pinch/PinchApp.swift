2a6a164c061d649d8c454112933aa1eca5430f58
Hasan Mohammed <hasanmohammed.hm1@gmail.com>
2024-03-30 13:48:04 +0300

    Initial Commit

---
+++ Pinch/PinchApp.swift
@@ -0,0 +1,17 @@
+//
+//  PinchApp.swift
+//  Pinch
+//
+//  Created by Hasan on 3/30/24.
+//
+
+import SwiftUI
+
+@main
+struct PinchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
